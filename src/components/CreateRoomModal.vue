a03a51d33fd2bfa26d21b4a55589a5261503bd70
helloim-hd <sitiherdanuryani@gmail.com>
2026-04-16 17:08:49 +0800

    feat: add pinia and handle users for room creation

--- src/components/CreateRoomModal.vue
+++ src/components/CreateRoomModal.vue
@@ -3,26 +3,32 @@
         <button type="button" @click="showModal" class="w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-6 py-3 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 cursor-pointer">Create Room</button>
 
         <fwb-modal v-if="isShowModal" @close="closeModal">
-            <!-- <template #header>
-            <div class="flex items-center text-lg">
-                Terms of Service
-            </div>
-            </template> -->
             <template #body>
                 <div class="mb-4">
                     <input v-model="roomName" type="text" placeholder="Room" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
                 <div>
-                    <NameList />
+                    <NameList 
+                        :current-user-name="name"
+                        :users="users"
+                        :added-users="addedUsers"   
+                        @add-user="addUser"
+                        @remove-user="removeUser"
+                    />
                 </div>
             </template>
             <template #footer>
             <div class="flex justify-between">
                 <fwb-button @click="closeModal" color="alternative">
-                Decline
+                    Close
                 </fwb-button>
-                <fwb-button @click="closeModal" color="blue">
-                I accept
+                <fwb-button 
+                    :disabled="isConfirmDisabled()" 
+                    color="purple"
+                    class="disabled:bg-gray-300"
+                    @click="createRoom"
+                >
+                    Create Room
                 </fwb-button>
             </div>
             </template>
@@ -31,11 +37,27 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import { FwbButton, FwbModal } from 'flowbite-vue'
 import NameList from './NameList.vue';
+import userService from '../services/user';
 
 const isShowModal = ref(false);
+const users = ref([]);
+const name = ref('');
+const addedUsers = ref([]);
+const roomName = ref('');
+
+onMounted(async () => {
+    const token = localStorage.getItem('token');
+    // TODO: refactor to put information in pinia? or local component is enough?
+    name.value = localStorage.getItem('name');
+    users.value = await userService.getUsers(token);
+})
+
+watch([roomName, addedUsers], () => {
+    isConfirmDisabled();
+}, {deep: true})
 
 function showModal() {
     isShowModal.value = true;
@@ -43,5 +65,23 @@ function showModal() {
 
 function closeModal() {
     isShowModal.value = false;
+}
+
+function addUser(data) {
+    addedUsers.value.push(data.user);
+    users.value.splice(data.index, 1);
+}
+
+function removeUser(data) {
+    users.value.push(data.user);
+    addedUsers.value.splice(data.index, 1);
+}
+
+function isConfirmDisabled() {
+    return addedUsers.value.length === 0 || !roomName.value;
+}
+
+function createRoom() {
+    
 }
 </script>
--- src/components/NameList.vue
+++ src/components/NameList.vue
@@ -6,11 +6,11 @@
         <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Click to add user to chat.</p>
         <ul class="my-4 space-y-3">
             <li v-for="(user, index) in users">
-                <fwb-button v-if="user.name != name" 
+                <fwb-button v-if="user.name != currentUserName" 
                     color="light" 
                     class="w-full cursor-pointer focus:ring-0 focus:ring-offset-0" 
                     size="lg"
-                    @click="onNameClick(user, index)"
+                    @click="$emit('addUser', {user, index})"
                 >
                     {{ user.name }}
                 </fwb-button>
@@ -26,14 +26,13 @@
                         color="light" 
                         class="w-full mr-3" 
                         size="lg"
-                        @click="onNameClick(user, index)"
                     >
                         {{ user.name }}
                     </fwb-button>
                     <fwb-button 
                         class="cursor-pointer bg-transparent hover:bg-transparent focus:ring-0 focus:ring-offset-0" 
                         outline-none
-                        @click="removeUser(user, index)"
+                        @click="$emit('removeUser', {user, index})"
                     >
                         <svg class="w-6 h-6 text-red-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
@@ -46,28 +45,15 @@
 </template>
 
 <script setup>
-import { FwbButton, FwbModal } from 'flowbite-vue'
-import { onMounted, ref } from 'vue';
-import userService from '../services/user';
+import { FwbButton } from 'flowbite-vue'
+import { defineProps, defineEmits } from 'vue';
 
-const users = ref([]);
-const name = ref('');
-const addedUsers = ref([]);
-
-onMounted(async () => {
-    const token = localStorage.getItem('token');
-    // TODO: refactor to put information in vuex?
-    name.value = localStorage.getItem('name')
-    users.value = await userService.getUsers(token);
+const props = defineProps({
+    users: Array,
+    addedUsers: Array,
+    currentUserName: String
 })
 
-function onNameClick(user, index) {
-    addedUsers.value.push(user);
-    users.value.splice(index, 1);
-}
+const emit = defineEmits(['addUser', 'removeUser'])
 
-function removeUser(user, index) {
-    users.value.push(user);
-    addedUsers.value.splice(index, 1);
-}
 </script>
